9c4fe494c5996e3718b21deea102715aa50efb91
Katsuya Fujii <katsuya.f0515@gmail.com>
2014-02-07 18:13:42 +0900

    iiii

--- pyTextDict.py
+++ pyTextDict.py
@@ -8,23 +8,40 @@ class MainWindow(wx.Frame):
        # self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.CreateStatusBar() # A StatusBar in the bottom of the window
 
-        #Setting up the panel (Left:Text Right:Dictionary)
-        panel = wx.Panel(self,wx.ID_ANY)
+        #Setting up the panel (Left:Text Right:Serach Bar + Dictionary)
+        root_panel = wx.Panel(self,wx.ID_ANY)
+        right_panel =wx.Panel(self,wx.ID_ANY)
+
+
         global TextField
-        TextField = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        TextField = wx.TextCtrl(root_panel, style=wx.TE_MULTILINE,size=(500,600))
+
         global DictionaryField
         style =   wx.TE_NO_VSCROLL | wx.TE_MULTILINE
-        DictionaryField = wx.TextCtrl(panel, style=style)
-
+        DictionaryField = wx.TextCtrl(right_panel, style=style,size=(500,570))
         font = wx.Font(20,wx.FONTFAMILY_DEFAULT,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_NORMAL)
         DictionaryField.SetFont(font)
         DictionaryField.Disable()
-        layout = wx.BoxSizer(wx.HORIZONTAL)
-        layout.Add(TextField,flag=wx.GROW,proportion=1)
-        layout.Add(DictionaryField,flag=wx.GROW,proportion=1)
+
+        global SerchField
+        SerchField=wx.TextCtrl(right_panel,wx.ID_ANY,size=(500,30))
+
+
+
+        right_layout=wx.BoxSizer(wx.VERTICAL)
+        right_layout.Add(SerchField,flag=wx.GROW)
+        right_layout.Add(DictionaryField,flag=wx.GROW,proportion=1)
+        right_panel.SetSizer(right_layout)
+        right_layout.Fit(right_panel)
+
+
+        root_layout = wx.BoxSizer(wx.HORIZONTAL)
+        root_layout.Add(TextField,flag=wx.GROW,proportion=1)
+        root_layout.Add(right_panel,flag=wx.GROW,proportion=1)
+
+        root_panel.SetSizer(root_layout)
+        root_layout.Fit(root_panel)
     
-    
-        panel.SetSizer(layout)
         
         #DictionaryField.SetValue(StringValue)
         global StringValue
@@ -47,6 +64,7 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         TextField.Bind(wx.EVT_KEY_UP,self.changeDictionary)
+        SerchField.Bind(wx.EVT_KEY_UP,self.searchDictionary)
         self.Show(True)
 
     def OnAbout(self,e):
@@ -58,11 +76,11 @@ class MainWindow(wx.Frame):
     def OnExit(self,e):
         self.Close(True)  # Close the frame.
 
+#入力されているkeyをspaceがうたれる前に配列で補完。spaceがうたれたらその単語をfindDictionaryメッソドに送る
     def changeDictionary(self,evt):
         keycode=evt.GetKeyCode()
         
-        if keycode == wx.WXK_SPACE or keycode==46 or keycode==44: #46 "."  44 ","
-            #DictionaryField.SetLabel(StringValue)
+        if keycode == wx.WXK_SPACE or keycode==46 or keycode==44: #space or 46 "." or 44 ","
             Output=""
             for i in range(len(StringValue)):
                 Output=Output+StringValue[i]
@@ -73,18 +91,25 @@ class MainWindow(wx.Frame):
         else:
            if keycode>64 and keycode<123:
                 StringValue.append(chr(keycode).lower())
+#Search　barの単語から
+    def searchDictionary(self,evt):
+        keycode=evt.GetKeyCode()
+        if keycode == wx.WXK_RETURN:
+            self.findDictionary(SerchField.GetValue().lower())
 
+#辞書から検索する
     def findDictionary(self,word):
         with open('ejdic-hand-utf8.txt', 'r') as fp:
             for line in fp:
-                #=re.compile(line.rstrip())
+               
                 line=line.rstrip()
                 if re.match(word,line):
 
                     DictionaryField.SetValue(line)
 
                     break
-
+                else:
+                    DictionaryField.SetValue('No result')
 app = wx.App(False)
 frame = MainWindow(None, "pyTextDictionary")
 app.MainLoop()
